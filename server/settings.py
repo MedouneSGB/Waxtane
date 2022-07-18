221e23d2d2ed3cea21999c471b771114669e32d1
Dar-rius <mohamedtine17@gmail.com>
2022-07-18 09:39:39 +0000

    ajout des testes

--- server/settings.py
+++ server/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- waxtane/tests.py
+++ waxtane/tests.py
@@ -1,3 +1,17 @@
+#importer les modules
 from django.test import TestCase
+from .models import Sentence
 
-# Create your tests here.
+#Creation des cas de testes 
+class Test_data(TestCase):
+    #Une fonction permettant de faire des testes sur les modeles
+    def test_sentences(self):
+        sentences = Sentence()
+        sentences.sentence_wolof = "teste 1"
+        sentences.sentence_fr = "teste 2"
+        sentences.sentence_en = "teste 3"
+
+
+        self.assertEqual( sentences.sentence_wolof, "teste 1")
+        self.assertEqual( sentences.sentence_fr, "teste 2")
+        self.assertEqual( sentences.sentence_en, "teste 3")
