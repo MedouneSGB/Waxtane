d4abe744d15a32e6c25f173391515b3e458aca37
Dar-rius <mohamedtine17@gmail.com>
2022-07-19 14:03:15 +0000

    fixed bug

--- server/settings.py
+++ server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
+#CSRF_TRUSTED_ORIGINS = ['','https://*.127.0.0.1']
 
 
 # Application definition
